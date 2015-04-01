a781593f137415d30cab69bb1666fea6f438ae1e
Mike Farrow <contact@mikefarrow.co.uk>
2015-04-01 20:23:54 +0100

    adding root password control

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,13 @@
 class user_accounts {
 
+	$root_password = hiera('accounts::users::root::password')
+
+	if $root_password != '' {
+		user { 'root':
+			password => $root_password,
+		}
+	}
+
 	class { 'accounts':
 		accounts   => hiera_hash('accounts::accounts', {}),
 		ssh_keys   => hiera_hash('accounts::ssh_keys', {}),
