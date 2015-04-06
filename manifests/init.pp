a3e8d0fc29f3b1b6999562038d7b7dbd73b697eb
Mike Farrow <contact@mikefarrow.co.uk>
2015-04-06 22:13:32 +0100

    adding require

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,9 +2,16 @@ class user_accounts {
 
 	$root_password = hiera('accounts::users::root::password', '')
 
+	if defined('ohmyzsh') {
+		$require = [ Package['zsh'] ]
+	} else {
+		$require = undef
+	}
+
 	if $root_password != '' {
 		user { 'root':
 			password => $root_password,
+			require  => $require,
 		}
 	}
 
